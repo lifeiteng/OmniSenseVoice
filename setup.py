534ec39d0c6cf04befaafb7e3140ecc8f8b9b7f7
Feiteng <lifeiteng0422@gmail.com>
2024-10-21 23:41:03 +0800

    version 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="OmniSenseVoice",
-    version="0.0.1",
+    version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     package_data={"": []},

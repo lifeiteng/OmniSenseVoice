a93f2d1b8d32d0a5ecdcaa7b209da1e52582641e
Feiteng <lifeiteng0422@gmail.com>
2024-11-05 23:22:56 +0800

    version v0.1.7

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ __download_url__ = "https://github.com/lifeiteng/OmniSenseVoice/releases"
 
 setup(
     name="OmniSenseVoice",
-    version="0.1.6",
+    version="0.1.7",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     package_data={"": []},
